748feadd66076c2164d80a4e49ca7ea6003a513d
tejaanuchuri0009@gmail.com <tejaanuchuri0009@gmail.com>
2021-07-02 17:57:38 +0530

    Add project files.

---
+++ MFCFormScratch/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream> 
+using namespace std;
+
+void main() {
+	cout << "***************************************\n";
+	cout << "MFC Application Tutorial";
+	cout << "\n***************************************";
+	return;
+}
